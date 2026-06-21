49b72fb70d71cdea241f17910ed6a08f3cfdcfa8
FaricJure <jure.faric@gmail.com>
2026-06-21 21:53:03 +0200

    feat: add admin metrics and reset endpoints with HTML responses
    Boot.dev HTTP Servers CH3: L3

---
+++ src/admin/reset.ts
@@ -0,0 +1,8 @@
+import type { Request, Response } from "express";
+import { config } from "../config.js";
+
+export async function handlerReset(_: Request, res: Response) {
+  config.fileserverHits = 0;
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send("Hits reset to 0");
+}
