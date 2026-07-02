67db973d01f37d67ccc6140b19dbeacca0ab05ef
FaricJure <jure.faric@gmail.com>
2026-07-02 14:55:47 +0200

    feat: add database configuration and user management
    - Updated package.json to include drizzle-kit for database migrations and ORM.
    - Enhanced config.ts to load database URL from environment variables.
    - Created SQL migration file for users table with unique email constraint.
    - Added metadata files for database schema and journal.
    - Implemented database connection in index.ts using drizzle-orm.
    - Developed user creation query in users.ts with conflict handling.
    - Defined user schema in schema.ts with appropriate types and constraints.
    
    CH6 L3

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,16 @@
 type APIConfig = {
   fileServerHits: number;
+  dbURL: string;
 };
+
+process.loadEnvFile();
+
+const dbURL = process.env.DB_URL;
+if (!dbURL) {
+  throw new Error("DB_URL is not set");
+}
 
 export const config: APIConfig = {
   fileServerHits: 0,
+  dbURL,
 };
